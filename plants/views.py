b174070599e03a3780fd875b6d3fb50dc0138e36
Jaroslav Petrovsky <petrovskyjaroslav@gmail.com>
2022-08-04 23:09:23 +0200

    template added

--- plants/views.py
+++ plants/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html')
 
 def registry(request):
     return HttpResponse('Registry')
